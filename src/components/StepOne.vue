732a0639384b362e047ba14e861f2f0f588c4df8
choi1k <20181024@dongduk.ac.kr>
2022-05-22 15:26:50 +0900

    Step1: Declarative Rendering

---
+++ src/components/StepOne.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  // component options
+  data(){
+    return {
+      message:'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{message.split('').reverse().join('')}}</h1>
+</template>
